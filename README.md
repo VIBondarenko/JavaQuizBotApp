cf7899c1ffc03c8c22716545d9c747f7fe4ef6fa
vibondarenko <vibondarenko@gmail.com>
2022-05-05 17:32:17 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Java Quiz Bot 
+Java Quiz Bot - telegram bot for check your Java knowledge.
+
+Copyright (c) 2022 Vitalii Bondarenko
---
+++ src/main/java/io/github/vibondarenko/JavaQuizBotApp.java
@@ -0,0 +1,13 @@
+package io.github.vibondarenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaQuizBotApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaQuizBotApp.class, args);
+	}
+
+}
