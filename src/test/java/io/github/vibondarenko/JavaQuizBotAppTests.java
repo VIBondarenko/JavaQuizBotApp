145e33b0f0ee9a0fa1fa5a5ef47d716f65019aa3
vibondarenko <vibondarenko@gmail.com>
2022-05-05 17:52:49 +0300

    first commit

---
+++ src/test/java/io/github/vibondarenko/JavaQuizBotAppTests.java
@@ -0,0 +1,13 @@
+package io.github.vibondarenko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaQuizBotAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
